5859ae5705243774c97ef0f6f2826a48dcb2f6ba
Filip Strömbäck <filip.stromback@liu.se>
2023-03-20 16:24:48 +0100

    Fixed compilation errors in lab1b.

--- src/standalone/lab1b/debug2.c
+++ src/standalone/lab1b/debug2.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
-#include "memory.h"
+#include <time.h>
 
 /* Recommended compile command:
  *
--- src/standalone/lab1b/debug3.c
+++ src/standalone/lab1b/debug3.c
@@ -2,7 +2,6 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
-#include "memory.h"
 
 /* Recommended compile command:
  *
